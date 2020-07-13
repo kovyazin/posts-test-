86017068ed576367aabc4ff4f4760003d9a14105
Slava Kovyazin <slava.kovazin@gmail.com>
2020-07-13 15:25:25 +0500

    feat: include redux

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import { Pages } from '@pages'
+import { store } from '@lib/store'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export const App = () => {
   return (
-    <Router>
-      <Pages />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Pages />
+      </Router>
+    </Provider>
   )
 }
